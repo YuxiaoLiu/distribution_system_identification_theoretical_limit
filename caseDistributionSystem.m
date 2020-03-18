ea4ac4d588847ee3c1608adf4e5f5985923d202c
YuxiaoLiu <liuyuxiao_tod@163.com>
2020-03-18 16:59:05 +0800

    run the power flow

--- caseDistributionSystem.m
+++ caseDistributionSystem.m
@@ -6,15 +6,18 @@ classdef caseDistributionSystem
         mpc                 % the matpower struct of the power system
         numBus              % the number of bus
         numSnap             % the number of snapshot
+        range               % % the deviation range
         
         addressLoadRaw      % the address of raw load data
         addressLoad         % the address of preprocessed load data
         
-        loadP                % the active load of each bus
+        loadP               % the active load of each bus
+        loadQ               % the reactive load of each bus
+        data                % the data struct contains operation data
     end
     
     methods
-        function obj = caseDistributionSystem(caseName, numSnap)
+        function obj = caseDistributionSystem(caseName, numSnap, range)
             % the construction function
             obj.caseName = caseName;
             obj.addressLoadRaw = '.\data\file1csv.csv';
@@ -24,6 +27,7 @@ classdef caseDistributionSystem
             obj.mpc = loadcase(caseName);
             obj.numBus = size(obj.mpc.bus, 1);
             obj.numSnap = numSnap;
+            obj.range = range;
             
             % read and process the raw load data
 %             readLoadRaw(obj);
@@ -31,11 +35,13 @@ classdef caseDistributionSystem
             % read and process the load data
             obj = readLoad(obj);
             
+            % generate the steady state operation data
+            obj = genOperateData(obj);
+            
         end
         
         function readLoadRaw(obj)
             % this method read and process the raw load data
-            
             
             numDay = 20;            % we read 20 days of load data
             numCustomer = 979;      % the number of costomer of the first day
@@ -80,9 +86,47 @@ classdef caseDistributionSystem
             end
             
             % cut the data
-            load(obj.numBus+1:end,:) = [];
+            load(obj.numBus:end,:) = []; % exclude the source bus
             load(:,obj.numSnap+1:end) = [];
+            
+            % rescale the data
+            load = 1 - obj.range.P/2 + load*obj.range.P;
             obj.loadP = load;
+            
+            % generate the reactive load data
+            rng(1);
+            randQ = rand(size(load)) * obj.range.Q + 1 - obj.range.Q/2;
+            obj.loadQ = load .* randQ;
+        end
+        
+        function obj = genOperateData(obj)
+            % this method generate the steady state operation data by
+            % running power flow equations
+            data_.P = zeros(obj.numBus, obj.numSnap);
+            data_.Q = zeros(obj.numBus, obj.numSnap);
+            data_.Vm = zeros(obj.numBus, obj.numSnap);
+            data_.Va = zeros(obj.numBus, obj.numSnap);
+            isSuccess = ones(obj.numSnap, 1);
+            
+            for i = 1:obj.numSnap
+                mpcThis = obj.mpc;
+                % update active and reactive load
+                mpcThis.bus(2:end,3) = mpcThis.bus(2:end,3) .* obj.loadP(:, i);
+                mpcThis.bus(2:end,4) = mpcThis.bus(2:end,4) .* obj.loadQ(:, i);
+                % run power flow
+                mpopt = mpoption('verbose',0,'out.all',0);
+                mpcThis = runpf(mpcThis, mpopt);
+                isSuccess(i, 1) = mpcThis.success;
+                % output the data
+                data_.P(:,i) = mpcThis.bus(:, 3);
+                data_.Q(:,i) = mpcThis.bus(:, 4);
+                data_.Vm(:,i) = mpcThis.bus(:, 8);
+                data_.Va(:,i) = mpcThis.bus(:, 9)/180*pi;
+            end
+            
+            % assert that all the power flow converge
+            assert(isempty(find(isSuccess == 0, 1)));
+            obj.data = data_;
         end
     end
 end
--- mainIdentificationLimit.m
+++ mainIdentificationLimit.m
@@ -4,5 +4,7 @@
 clc; clear;
 caseName = 'case33bw';     % the case name
 numSnap = 50;              % the number of snapshot
+range.P = 0.6;             % the deviation range of active load
+range.Q = 0.2;             % the deviation range of reactive load to active load
 %% Then we generalize the power flow data
-caseDS = caseDistributionSystem(caseName, numSnap);
+caseDS = caseDistributionSystem(caseName, numSnap, range);
