8579639a8dc60536150ece68f394e46a7a81a630
YuxiaoLiu <liuyuxiao_tod@163.com>
2020-03-24 20:39:53 +0800

    modify the matrix inverse

--- mainIdentificationLimit.m
+++ mainIdentificationLimit.m
@@ -4,9 +4,15 @@
 %% We set some hyper parameters
 clc; clear;
 caseName = 'case33bw';     % the case name    'case3_dist' 'case33bw'
-numSnap = 30;              % the number of snapshot
+numSnap = 120;              % the number of snapshot
 range.P = 0.6;             % the deviation range of active load
 range.Q = 0.2;             % the deviation range of reactive load to active load
+
+% the accuracy of measurement device
+ratio.P = 0.005;
+ratio.Q = 0.005;
+ratio.Vm = 0.001; % 0.0000001 0.000001--the maximum error
+ratio.Va = 0.001;
 
 profile on;
 %% We generate the power flow data
@@ -17,7 +23,9 @@ caseDS = caseDS.genOperateData;
 %% We evaluate the bound
 % set the accuracy of the measurement device, and set whether we have the
 % measurement device of a certain state
-caseDS = caseDS.setAccuracy;
+caseDS = caseDS.setAccuracy(ratio);
 caseDS = caseDS.buildFIM;
+caseDS = caseDS.calBound;
+caseDS = caseDS.outputBound;
 profile off;
 profile viewer;
