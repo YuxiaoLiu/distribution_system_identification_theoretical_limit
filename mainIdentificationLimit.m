8bd41060bae27f163dae72f0e7d77f8176d41fe0
YuxiaoLiu <liuyuxiao_tod@163.com>
2020-03-29 12:47:21 +0800

    add the k to solve the numerical issue

--- mainIdentificationLimit.m
+++ mainIdentificationLimit.m
@@ -15,7 +15,18 @@ ratio.Vm = 0.0001; % 0.0000001 0.000001--the maximum error
 ratio.Va = 0.0001;
 
 % if we only compute the bound of admittance matrix
-admittanceOnly = true;
+admittanceOnly = false;
+
+% the enlarge factor to maintain the numerical stability
+switch caseName
+    case 'case33bw'
+        k.vm = 100;
+        k.va = 1000;
+    otherwise
+        k.vm = 50;
+        k.va = 1000;
+end
+
 
 profile on;
 %% We generate the power flow data
@@ -27,7 +38,7 @@ caseDS = caseDS.genOperateData;
 % set the accuracy of the measurement device, and set whether we have the
 % measurement device of a certain state
 caseDS = caseDS.setAccuracy(ratio);
-caseDS = caseDS.buildFIM;
+caseDS = caseDS.buildFIM(k);
 caseDS = caseDS.calBound(admittanceOnly);
 % caseDS = caseDS.outputBound;
 profile off;
