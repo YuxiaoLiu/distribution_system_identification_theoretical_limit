01b928b96f77e1d23e9ccbd42adaa35ffafd0b5e
YuxiaoLiu <liuyuxiao_tod@163.com>
2020-04-20 18:33:53 +0800

    The topo is good

--- mainIdentificationLimit.m
+++ mainIdentificationLimit.m
@@ -12,7 +12,7 @@ range.Q = 0.3;             % the deviation range of reactive load to active load
 % the accuracy of measurement device
 ratio.P = 0.001;%0.005
 ratio.Q = 0.001;
-ratio.Vm = 0.001;%0.0000005;--the maximum error
+ratio.Vm = 0.00001;%0.0000005;--the maximum error
 ratio.Va = 0.001;%0.000005
 
 % if we only compute the bound of admittance matrix
