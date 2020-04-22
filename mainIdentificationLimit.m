f73d05f5c83585cdde56afcd32d8634f50164a7d
YuxiaoLiu <liuyuxiao_tod@163.com>
2020-04-22 20:52:50 +0800

    begin to tune parameters

--- mainIdentificationLimit.m
+++ mainIdentificationLimit.m
@@ -10,10 +10,10 @@ range.P = 1.2;               % the deviation range of active load 0.6
 range.Q = 0.3;             % the deviation range of reactive load to active load 0.2
 
 % the accuracy of measurement device
-ratio.P = 0.005;%0.005
-ratio.Q = 0.005;
+ratio.P = 0.001;%0.005
+ratio.Q = 0.001;
 ratio.Vm = 0.005;%0.0000005;--the maximum error
-ratio.Va = 0.005;%0.000005
+ratio.Va = 0.001;%0.000005
 
 % if we only compute the bound of admittance matrix
 admittanceOnly = false;
